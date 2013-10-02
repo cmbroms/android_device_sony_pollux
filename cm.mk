22885b61d822d09cc4e48f1c0e5c2da05c25c265
nycbjr <nycbjr@gmail.com>
2013-10-01 21:56:26 -0400

    new branch

--- cm.mk
+++ cm.mk
@@ -1,15 +1,15 @@
 $(call inherit-product, device/sony/pollux/full_pollux.mk)
 
 # Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cmb/config/gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cmb/config/nfc_enhanced.mk)
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cmb/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP321 BUILD_FINGERPRINT=Sony/SGP321_1275-5841/SGP321:4.1.2/10.1.1.A.1.253/Avt3rw:user/release-keys PRIVATE_BUILD_DESC="SGP321-user 4.1.2 10.1.1.A.1.253 Avt3rw test-keys"
 
-PRODUCT_NAME := cm_pollux
+PRODUCT_NAME := cmb_pollux
 PRODUCT_DEVICE := pollux
